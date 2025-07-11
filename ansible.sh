ff2e9916721cfc6d6e3821995ac5febd3a16f08e
internctx02 <internctx02@cmc.com.vn>
2025-07-11 14:25:01 +0700

    add an sh file

---
+++ ansible.sh
@@ -0,0 +1,18 @@
+sudo apt update && sudo apt upgrade -y
+sudo apt install wget gpg -y
+
+UBUNTU_CODENAME=jammy
+wget -O- "https://keyserver.ubuntu.com/pks/lookup?fingerprint=on&op=get&search=0x6125E2A8C77F2818FB7BD15B93C4A3FD7BB9C367" | sudo gpg --dearmour -o /usr/share/keyrings/ansible-archive-keyring.gpg
+
+echo "deb [signed-by=/usr/share/keyrings/ansible-archive-keyring.gpg] http://ppa.launchpad.net/ansible/ansible/ubuntu $UBUNTU_CODENAME main" | sudo tee /etc/apt/sources.list.d/ansible.list
+
+sudo apt update && sudo apt install -y ansible
+
+cd /home/$USER
+git clone https://github.com/Jackhuy1/my-ansible-repo.git
+
+cd my-ansible-repo
+
+ansible-playbook -i inventory.ini playbook.yaml
+ansible-playbook -i inventory.ini playbook-mysql.yaml
+ansible-playbook -i inventory.ini playbook-restore-db.yaml
