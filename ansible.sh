c5f40faf0b9dd395210116f35b345b6c9c43d290
Jackhuy1 <91598544+Jackhuy1@users.noreply.github.com>
2025-07-11 15:47:58 +0700

    Update ansible.sh

--- ansible.sh
+++ ansible.sh
@@ -5,7 +5,7 @@ sudo apt-get install apt-utils -y
 
 sudo apt update && sudo apt upgrade -y
 
-sudo apt install -y wget gpg git
+sudo apt install -y wget gpg git stress
 
 export UBUNTU_CODENAME=jammy
 wget -O- "https://keyserver.ubuntu.com/pks/lookup?fingerprint=on&op=get&search=0x6125E2A8C77F2818FB7BD15B93C4A3FD7BB9C367" | sudo gpg --dearmour -o /usr/share/keyrings/ansible-archive-keyring.gpg
